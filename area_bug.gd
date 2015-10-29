8ea5b0d3439520bd4ae46077e162adc252d6e903
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2015-10-29 12:58:39 +0100

    Initial commit

---
+++ area_bug.gd
@@ -0,0 +1,16 @@
+
+extends Node2D
+
+var _Shape1
+
+func _ready():
+	_Shape1 = get_node("Area1").get_shape(0)
+	_Shape1.set_radius(_Shape1.get_radius())
+	_Shape1.set_radius(_Shape1.get_radius())
+	_Shape1.set_radius(_Shape1.get_radius())
+	_Shape1.set_radius(_Shape1.get_radius())
+	_Shape1.set_radius(_Shape1.get_radius())
+	_Shape1.set_radius(_Shape1.get_radius())
+	_Shape1.set_radius(_Shape1.get_radius())
+
+
